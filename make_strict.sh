713ee4fd624a83dd16ac099fa26326d0e02ded3c
Bartosz Chaber <chaberb@iem.pw.edu.pl>
2013-03-11 08:27:58 +0100

    Removed another non-strict funcs (2)

--- make_strict.sh
+++ make_strict.sh
@@ -2,4 +2,5 @@
 for f in `find src -name '*.js'`; do
   sed -i -e 's/\([,=:"return"]\) function .*(/\1 function(/g' $f
   sed -i -e 's/(function .*(/(function(/g' $f
+  sed -i -e 's/\[function .*(/(function(/g' $f
 done
