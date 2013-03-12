3c79fdfe9ff23fb75d9428868b95fcfcd40d8904
Bartosz Chaber <chaberb@iem.pw.edu.pl>
2013-03-12 22:28:35 +0100

    http://imgs.xkcd.com/comics/perl_problems.png

--- make_strict.sh
+++ make_strict.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 for f in `find src -name '*.js'`; do
-  sed -i -e 's/\([,=:"return"]\) function .*(/\1 function(/g' $f
-  sed -i -e 's/(function .*(/(function(/g' $f
-  sed -i -e 's/\[function .*(/(function(/g' $f
+  perl -0777 -i.original -pe 's/(\(|\[|,|:|=|return)(\n)?(\s*?)function .*?\(/$1$2$3function\(/g' $f
 done
